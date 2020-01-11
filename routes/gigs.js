d16a93e88fde2b4cbb2d3557e2f76ab44c9329db
Amrendra Kumar <amarmca51@gmail.com>
2020-01-11 10:37:07 +0530

    Added Hard coded data to PostGreSql Data base

--- routes/gigs.js
+++ routes/gigs.js
@@ -10,4 +10,28 @@ router.get('/', (req, res) =>
   .then(gigs => {console.log(gigs), res.sendStatus(200)})
     .catch(err => console.log(err)));
 
+// Add a gig
+router.get('/add', (req,res) => {
+    const data = {
+        title: 'Looking for Angular Js Developer',
+        technologies : 'Html, CSS, JavaScript, Angular, NodeJS',
+        description : 'The condidate should knowledge and expreience above technologies',
+        budget : '$2000',
+        contact_email: 'user2@mail.com'
+    }
+
+    let { title, technologies, description, budget, contact_email } =data;
+
+    //Insert into table
+    Gig.create({
+        //since , variable for db and schema are same so need to write like title = title
+        title,
+        technologies,
+        description,
+        budget,
+        contact_email
+    })
+    .then(gig => res.redirect('/gigs'))
+    .catch(err => console.log(err))
+})    
 module.exports = router;
